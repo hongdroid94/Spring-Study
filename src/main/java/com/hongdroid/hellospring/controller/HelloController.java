2815fb597740aea5fdf1713916c8e709a2c1fd22
hongdroid94 <hongdroid94@gmail.com>
2023-08-15 02:16:51 +0900

    feat. api를 내려주는 기본 방식 구현 (response body)

--- src/main/java/com/hongdroid/hellospring/controller/HelloController.java
+++ src/main/java/com/hongdroid/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -20,5 +21,33 @@ public class HelloController {
     public String helloMvc(@RequestParam("name") String name, Model model) {
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name; // hello spring
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody // 이 어노테이션을 작성하기만해도 return이 모델클래스 형태로 해줄 경우 기본적으로 json 객체로 변환되어 결과값을 제공한다.
+                  // 만약 없다면 viewResolver 시스템에 의해 html 뷰랑 연계를 시도하게 된다.
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    // sample class 생성
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
